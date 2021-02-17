defb6d351224d1149add33594dda8205b7c56049
MGallivan00 <maria.gallivan@gmail.com>
2021-02-16 17:45:37 -0700

    Motion detect generally works, not very accurate

--- MotionDetect.py
+++ MotionDetect.py
@@ -8,51 +8,67 @@ def nothing(x):
 
 # TODO: 1) Capture an image
 cap = cv.VideoCapture(0)
-cv.namedWindow("Video")
 status, frame = cap.read()
 
 # TODO: 2) Create blank image
 width, height, channels = frame.shape
-
 # TODO: 2a. Grayscale Image with proper dimensions
-src = cv.cvtColor(frame, cv.COLOR_BGR5652GRAY)
-cv.imshow("Grayscale", src)
-
 bw = np.zeros((width, height, 1), np.uint8)
-cv.imshow("BW", bw)
-
 # TODO: 2b. 32f, 3 channel image
 imageForAve = np.zeros((width, height, 3), np.float32)
 
-# TODO: 2c. a capture clone called image1
-image1 = frame.copy()
-
-# TODO: 2d. Absolute difference function
-diff = cv.absdiff(frame, image1)
-status, src = cap.read()
-cv.imshow("Video", src)
+# used to brighten the image
+bright = 35 * np.ones((width, height, 3), np.uint8)
 
 # TODO: 3) while loop to capture images
 while True:
+    # TODO: 4) grab new frame
+    status, frame = cap.read()
+    image1 = frame.copy()
 
+    # TODO: brighten image a bit
+    # frame = frame*bright
 
-# TODO: 4) grab new frame
-# TODO: brighten image a bit
-    bright = 35 * np.ones((width, height, 3), np.uint8)
-# TODO: 5) blue the image
-# frame = cv.blur(frame)
+    # TODO: 5) blur the image
+    frame = cv.blur(frame, (21, 21), 0)
 
-# TODO: 6) take the running average of frame
-# cv.accumulateWeighted(frame, frame)
-# TODO: 7) swap running average results from step 6 to same bits as frame:
+    # TODO: 6) take the running average of frame
+    accweight = cv.accumulateWeighted(frame, imageForAve, .04)
 
-# TODO: 8) Take difference, built in OpenCV function to do a diff between two images
-# TODO: 9) Convert to grayscale
+    # TODO: 7) swap running average results from step 6 to same bits as frame:
+    avg = cv.convertScaleAbs(imageForAve)
 
-# TODO: 10) Threshold grayscale (using low numbers)
-# TODO: 11) Blur grayscale image
-# TODO: 12) Threshold grayscale again (using high numbers)
-# TODO: 13) find contours
+    # TODO: 8) Take difference, built in OpenCV function to do a diff between two images
+    diff = cv.absdiff(frame, avg)
+
+    # TODO: 9) Convert to grayscale
+    gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+    cv.imshow("Grayscale", gray)
+
+    # TODO: 10) Threshold grayscale (using low numbers)
+    _, bw = cv.threshold(gray, 25, 255, cv.THRESH_BINARY)
+    cv.imshow("Threshold Low", bw)
+
+    # TODO: 11) Blur grayscale image
+    # gray = cv.blur(bw, (21, 21), 0)
+
+    # TODO: 12) Threshold grayscale again (using high numbers)
+    _, bw = cv.threshold(gray, 75, 255, cv.THRESH_BINARY)
+    cv.imshow("Threshold High", bw)
+
+    # TODO: 13) find contours
+    contours, _ = cv.findContours(bw, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+    # cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+    for contour in contours:
+        if cv.contourArea(contour) < 10000:
+            (x, y, w, h) = cv.boundingRect(contour)
+            cv.rectangle(image1, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv.imshow("Contours", image1)
+
     if cv.waitKey(1) & 0xFF == ord("q"):
         cv.destroyAllWindows()
         break
+
+cap.release()
