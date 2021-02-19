22de8d16f1e75bb1ba74f805fde44c37aa2cf41c
MGallivan00 <maria.gallivan@gmail.com>
2021-02-18 20:06:28 -0700

    formatting fixes

--- MotionDetect.py
+++ MotionDetect.py
@@ -1,10 +1,6 @@
-import random as rng
-import argparse
 import cv2 as cv
 import numpy as np
 
-def nothing(x):
-    pass
 
 # TODO: 1) Capture an image
 cap = cv.VideoCapture(0)
@@ -24,48 +20,47 @@ bright = 35 * np.ones((width, height, 3), np.uint8)
 while True:
     # TODO: 4) grab new frame
     status, frame = cap.read()
-    image1 = frame.copy()
 
     # TODO: brighten image a bit
     # frame = frame*bright
 
     # TODO: 5) blur the image
-    frame = cv.blur(frame, (21, 21), 0)
+    gray = cv.GaussianBlur(frame, (21, 21), 0)
 
     # TODO: 6) take the running average of frame
-    accweight = cv.accumulateWeighted(frame, imageForAve, .04)
+    cv.accumulateWeighted(gray, imageForAve, .04)
 
     # TODO: 7) swap running average results from step 6 to same bits as frame:
     avg = cv.convertScaleAbs(imageForAve)
 
     # TODO: 8) Take difference, built in OpenCV function to do a diff between two images
-    diff = cv.absdiff(frame, avg)
+    diff = cv.absdiff(gray, avg)
 
     # TODO: 9) Convert to grayscale
-    gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
-    cv.imshow("Grayscale", gray)
+    g = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+    cv.imshow("Grayscale", g)
 
     # TODO: 10) Threshold grayscale (using low numbers)
-    _, bw = cv.threshold(gray, 25, 255, cv.THRESH_BINARY)
+    _, bw = cv.threshold(g, 30, 255, cv.THRESH_BINARY)
     cv.imshow("Threshold Low", bw)
 
     # TODO: 11) Blur grayscale image
     # gray = cv.blur(bw, (21, 21), 0)
 
     # TODO: 12) Threshold grayscale again (using high numbers)
-    _, bw = cv.threshold(gray, 75, 255, cv.THRESH_BINARY)
+    _, bw = cv.threshold(g, 100, 255, cv.THRESH_BINARY)
     cv.imshow("Threshold High", bw)
 
     # TODO: 13) find contours
     contours, _ = cv.findContours(bw, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
-    # cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
+    # cv.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
     for contour in contours:
-        if cv.contourArea(contour) < 10000:
+        if cv.contourArea(contour) < 100000:
             (x, y, w, h) = cv.boundingRect(contour)
-            cv.rectangle(image1, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-    cv.imshow("Contours", image1)
+    cv.imshow("Contours", frame)
 
     if cv.waitKey(1) & 0xFF == ord("q"):
         cv.destroyAllWindows()
