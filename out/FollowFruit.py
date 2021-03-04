332a2c63585b7ce4a909c4c165fdd4d0e489f5b4
MGallivan00 <maria.gallivan@gmail.com>
2021-03-04 12:10:33 -0700

    Forward and Back working

--- out/FollowFruit.py
+++ out/FollowFruit.py
@@ -27,10 +27,10 @@ def getValues(event, x, y, flags, param):
         colorsS = hsv[y, x, 1]
         colorsV = hsv[y, x, 2]
         colors = hsv[y, x]
-        print("Hue: ", colorsH)
-        print("Saturation: ", colorsS)
-        print("Lightness: ", colorsV)
-        print("BRG Format: ", colors)
+        print("Hue: \t", colorsH)
+        print("Saturation: \t", colorsS)
+        print("Lightness: \t", colorsV)
+        print("BRG Format: \t", colors)
         print("Coordinates of pixel: X: ", x, "Y: ", y)
     return
 
@@ -47,17 +47,41 @@ cam.enable(timestep)
 cv.namedWindow("Video")
 cv.setMouseCallback("Video", getValues)
 
-once = True
 while robot.step(timestep) != -1:
-    leftMotor.setVelocity(2.6)
-    rightMotor.setVelocity(2.6)
     img = cam.getImage()
     width = cam.getWidth()
     height = cam.getHeight()
     temppic = np.frombuffer(img, np.uint8)
     out = np.reshape(temppic, (height, width, 4))
     cv.imshow("Video", out)
-    pass
 
-if cv.waitKey(1) & 0xFF == ord("q"):
-    cv.destroyAllWindows()
+    #Creates a threshold for the orange colors
+    hsv = cv.cvtColor(out, cv.COLOR_BGR2HSV)
+    min = np.array([19, 132, 138])
+    max = np.array([21, 150, 155])
+
+    mask = cv.inRange(hsv, min, max)
+
+    res = cv.bitwise_and(out, out, mask=mask)
+
+    #calculates number of pixels in the threshold
+    n_white_pix = np.sum(res == 255)
+    print('Number of white pixels:', n_white_pix)
+
+    #Moves the robot forward and backwards
+    if n_white_pix > 820:
+        print('Move backwards')
+        leftMotor.setVelocity(-2.0)
+        rightMotor.setVelocity(-2.0)
+
+    if n_white_pix < 420:
+        print('Move forwards')
+        leftMotor.setVelocity(2.0)
+        rightMotor.setVelocity(2.0)
+
+    k = cv.waitKey(1)
+    if k == 27:
+        break
+
+cv.destroyAllWindows()
+
