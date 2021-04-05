e61b5d3230a7aaa05c8700484d5697e94b68e6dc
MGallivan00 <54639309+MGallivan00@users.noreply.github.com>
2021-04-05 16:49:00 -0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,58 +1,111 @@
 package com.example.facialrecognition;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.os.Message;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
-import android.widget.Button;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.Frame;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+
+import java.io.InputStream;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TTS tts;
-    String msg;
+    List<Face> fac;
+    TextView tv;
+    int count = 0;
+    TTS tts;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button practice = findViewById(R.id.Tutorial);
-        practice.setOnClickListener(this);
+        tv = findViewById(R.id.textView);
+        Button talkButton = findViewById(R.id.talkButton);
+        talkButton.setOnClickListener(this);
+        tts = TTS.getInstance(this);
+        tts.start();
 
+//        InputStream stream = getResources().openRawResource(R.raw.image01);
+//        InputStream stream = getResources().openRawResource(R.raw.image02);
+//        InputStream stream = getResources().openRawResource(R.raw.images05);
+        InputStream stream = getResources().openRawResource(R.raw.japaneseflowers);
+
+
+
+        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        FaceDetector detector = FaceDetection.getClient();
+
+
+        Task<List<Face>> result =
+                detector.process(image).addOnSuccessListener(
+
+                        new OnSuccessListener<List<Face>>() {
+                            @Override
+                            public void onSuccess(List<Face> faces) {
+                                fac = faces;
+                                if (fac == null)
+                                    Log.v("***DRAW***", "Null");
+                                FaceView overlay = (FaceView) findViewById(R.id.faceView);
+                                overlay.setContent(bitmap, fac);
+                                //Success
+                                tv.setText(faces.size() + " Faces Seen");
+                                count = faces.size();
+//                                startSecondActivity();
+                            }
+                        }).addOnFailureListener(new OnFailureListener(){
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+
+
+                });
     }
 
     public void onClick(View v){
         switch(v.getId()){
-            case R.id.Tutorial:
-                Toast.makeText(this,"Hello", Toast.LENGTH_LONG).show();
+            case R.id.talkButton:
                 startSecondActivity();
                 break;
         }
     }
 
     public void startSecondActivity(){
-        Intent secondActivity = new Intent(this, TalkActivity.class);
-        startActivity(secondActivity);
-//        TTS talk = TTS.getInstance(this);
-//        talk.start();
 
-//        Bundle b = new Bundle();
-//        b.putString("LM", "Test");
-//        if (tts.handler != null){
-//            Message msg = tts.handler.obtainMessage(0);
-//            msg.setData(b);
-//            tts.handler.sendMessage(msg);
-//        }
+        Bundle b = new Bundle();
+        b.putString("LM", "There are " + count + " faces in the photo");
+        if (tts.handler != null){
+            Message msg = tts.handler.obtainMessage(0);
+            msg.setData(b);
+            tts.handler.sendMessage(msg);
+        }
     }
 
-    protected void onResume(){
-        super.onResume();
-    }
-
-    protected void onDelete(){
-        super.onDestroy();
-    }
 }
